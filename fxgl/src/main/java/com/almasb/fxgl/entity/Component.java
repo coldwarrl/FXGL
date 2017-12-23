0d93b4192d245960c98b637cd7ededd08a0b6100
coldwarrl <marco.frg@gmail.com>
2017-12-23 20:00:22 +0100

    added identifier to ecs

--- fxgl/src/main/java/com/almasb/fxgl/entity/Component.java
+++ fxgl/src/main/java/com/almasb/fxgl/entity/Component.java
@@ -10,4 +10,18 @@ package com.almasb.fxgl.entity;
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
 public abstract class Component extends Module {
+
+    public String id = "";
+
+    public Component() { }
+
+
+    public Component(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return id.isEmpty() ? super.toString(): id;
+    }
 }
