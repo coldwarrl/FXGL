243b3960935bcd8213704c58ac5d26b6fc1f3ade
coldwarrl <marco.frg@gmail.com>
2017-12-24 16:25:28 +0100

    registered TornadoFx to GameApplication + prototype of custom menu in dev ui

--- fxgl/src/main/java/com/almasb/fxgl/app/GameApplication.java
+++ fxgl/src/main/java/com/almasb/fxgl/app/GameApplication.java
@@ -42,8 +42,14 @@ import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.beans.property.ReadOnlyLongWrapper;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Menu;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import kotlin.reflect.KClass;
+import org.jetbrains.annotations.NotNull;
+import tornadofx.App;
+import tornadofx.FX;
+import tornadofx.UIComponent;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -101,6 +107,8 @@ public abstract class GameApplication extends Application {
 
             showPreloadingStage();
             startFXGL();
+            FX.registerApplication(this, stage);
+
         } catch (Exception e) {
             handleFatalErrorBeforeLaunch(e);
         }
@@ -683,6 +691,8 @@ public abstract class GameApplication extends Application {
     public final GameScene getGameScene() {
         return playState.getGameScene();
     }
+
+    public Menu developerCustomMenu;
 
     public final Gameplay getGameplay() {
         return FXGL.getGameplay();
