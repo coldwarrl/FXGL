0d716401c25bc94709f721b9ecec4177e621d535
coldwarrl <marco.frg@gmail.com>
2017-12-27 20:33:45 +0100

    making GameWorld extensible

--- fxgl/src/main/java/com/almasb/fxgl/app/GameApplication.java
+++ fxgl/src/main/java/com/almasb/fxgl/app/GameApplication.java
@@ -93,6 +93,8 @@ public abstract class GameApplication extends Application {
     private ReadOnlyGameSettings settings;
     private AppStateMachine stateMachine;
 
+
+
     public GameWorld getInjectableGameWorld() {
         return injectableGameWorld;
     }
