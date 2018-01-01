fefd3f3b3faec9d97f26210d5d4b5155366587f4
coldwarrl <marco.frg@gmail.com>
2018-01-01 13:00:05 +0100

    making GameWorld extensible

--- fxgl-base/src/main/java/com/almasb/fxgl/entity/Entity.java
+++ fxgl-base/src/main/java/com/almasb/fxgl/entity/Entity.java
@@ -86,15 +86,6 @@ public class Entity {
         addComponent(view);
     }
 
-    public Entity(String id, String name, String modelId)
-    {
-        this();
-        this.id = id;
-        this.name = name;
-        this.modelId = modelId;
-    }
-
-
     /**
      * @return the world this entity is attached to
      */
