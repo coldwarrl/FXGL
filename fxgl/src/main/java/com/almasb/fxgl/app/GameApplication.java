03ab289b15b986fc8ac0d19ef0c3b2630d821323
coldwarrl <marco.frg@gmail.com>
2018-01-06 20:39:06 +0100

    mocking of game world / app

--- fxgl/src/main/java/com/almasb/fxgl/app/GameApplication.java
+++ fxgl/src/main/java/com/almasb/fxgl/app/GameApplication.java
@@ -46,9 +46,6 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Menu;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import kotlin.reflect.KClass;
-import org.jetbrains.annotations.NotNull;
-import tornadofx.App;
 import tornadofx.FX;
 import tornadofx.UIComponent;
 import tornadofx.FX;
@@ -92,6 +89,16 @@ public abstract class GameApplication extends Application {
     private MainWindow mainWindow;
     private ReadOnlyGameSettings settings;
     private AppStateMachine stateMachine;
+
+    public GameWorld getInjectableGameWorld() {
+        return injectableGameWorld;
+    }
+
+    public void setInjectableGameWorld(GameWorld injectableGameWorld) {
+        this.injectableGameWorld = injectableGameWorld;
+    }
+
+    private GameWorld injectableGameWorld; //used in mocking
 
 
 
